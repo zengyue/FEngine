cddacec5b1556aa6866c5020e71f74fb6add9db2
索丘 <zengyue.yezy@alibaba-inc.com>
2023-06-21 14:10:17 +0800

    feat(actions): 添加 auto release

---
+++ .releaserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'semantic-release-monorepo',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore(release): 🤖 ${nextRelease.gitTag} [skip ci]',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
